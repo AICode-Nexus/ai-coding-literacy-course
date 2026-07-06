1cc191abb4efa4f08d42f3fe2b2ce62d477c6b9f
ydliu2 <ydliu2@iflytek.com>
2026-07-06 18:19:43 +0800

    fix: preserve workflow action plan compatibility

--- src/data/workflow.js
+++ src/data/workflow.js
@@ -1,1 +1,7 @@
-export { collaborationSteps, sevenDayTraining as sevenDayPlan } from "./workshop.js";
+import { collaborationSteps, sevenDayTraining } from "./workshop.js";
+
+export { collaborationSteps, sevenDayTraining };
+
+export const sevenDayPlan = sevenDayTraining.map(
+  (day) => `第 ${day.day} 天：${day.task}`,
+);
